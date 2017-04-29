d925a86c294efa680c69384c286995056dd6f3c1
Kilmyashkin <akilmyashkin@gmail.com>
2017-04-29 15:15:46 +0300

    Add menu

--- es6/main.js
+++ es6/main.js
@@ -1,3 +1,8 @@
+import "../css/menu.scss"
 import "../css/main.scss"
 
-console.log("ola")
+import { Menu } from "./menu.js"
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  new Menu()
+});
---
+++ es6/menu.js
@@ -0,0 +1,38 @@
+export class Menu {
+  constructor(){
+    this.bindUIactions()
+  }
+
+  bindUIactions(){
+    const menu = document.getElementById('theMenu')
+    const menuToggle = document.getElementById('menuToggle')
+    const body = document.body
+
+    menuToggle.addEventListener("click", () => {
+      if (this.hasClass(menuToggle, 'active')) {
+        this.removeClass(menu, "menu-open")
+        this.removeClass(menuToggle, "active")
+        this.removeClass(body, "body-push-toleft")
+      } else {
+        this.addClass(menu, "menu-open")
+        this.addClass(menuToggle, "active")
+        this.addClass(body, "body-push-toleft")
+      }
+    })
+  }
+
+  addClass(el, cls){
+    el.className += ' '+cls;
+  }
+
+  removeClass(el, cls){
+    var elClass = ' '+el.className+' ';
+    while(elClass.indexOf(' '+cls+' ') != -1)
+         elClass = elClass.replace(' '+cls+' ', '');
+    el.className = elClass;
+  }
+
+  hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+  }
+}
