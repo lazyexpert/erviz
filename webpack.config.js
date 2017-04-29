bcf1a4ed3d3d3ae1ba3372a9c01687e3c3ef6461
Kilmyashkin <akilmyashkin@gmail.com>
2017-04-29 14:17:58 +0300

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var webpack = require('webpack');
+var extractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: ['babel-polyfill', './es6/main.js'],
+    output: {
+        path: 'public',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: path.join(__dirname, 'es6'),
+                query: {
+                  presets: 'es2015',
+                },
+            },
+            {
+              test: /\.scss$/,
+              loader: extractTextPlugin.extract('css!sass')
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=../images/[name].[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin(),
+        new extractTextPlugin("./css/[name].css"),
+        new webpack.optimize.UglifyJsPlugin({
+          include: /bundle.js$/,
+          minimize: true
+        })
+    ],
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    // Create Sourcemaps for the bundle
+    // devtool: 'source-map',
+};
