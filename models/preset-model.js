7e9c3224efb9907b68df2cfcf35ec7eb8bfbe5df
Oleksandr Hatskalev <seo-mix@mail.ru>
2017-04-30 09:03:59 +0300

    saving preset to db

--- models/preset-model.js
+++ models/preset-model.js
@@ -1,9 +1,19 @@
 const ModelBase = require('./base-model');
+const Schema = require('mongoose').Schema;
 
 const modelFactory = connection => {
-  return connection.model('Preset', {
-    
+  const schema = new Schema({
+    "meta": {
+      "title": String,
+      "createdAt": {
+        type: Date,
+        default: Date.now
+      },
+      "description": String
+    },
+    "mySchema": {}
   });
+  return connection.model('Preset', schema);
 };
 
 module.exports = class PresetModel extends ModelBase {
